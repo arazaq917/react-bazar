9a6d45d7024b74a8567877a6b60e0cce86745d62
Osama <osamakhn97@gmail.com>
2022-12-12 23:29:46 +0500

    recent products added

--- src/pages-sections/giftshop/GiftShopPopularItems.jsx
+++ src/pages-sections/giftshop/GiftShopPopularItems.jsx
@@ -3,7 +3,8 @@ import Carousel from "components/carousel/Carousel";
 import CategorySectionCreator from "components/CategorySectionCreator";
 import ProductCard16 from "components/product-cards/ProductCard16";
 import useWindowSize from "hooks/useWindowSize";
-import React, { useEffect, useState } from "react"; // =========================================================
+import React, { useEffect, useState } from "react";
+import axios from "axios"; // =========================================================
 
 // =========================================================
 const GiftShopPopularItems = ({ productsData }) => {
@@ -16,8 +17,26 @@ const GiftShopPopularItems = ({ productsData }) => {
     else if (width < 950) setVisibleSlides(4);
     else setVisibleSlides(4);
   }, [width]);
+    const [data,setData] = useState([])
+    const domainUrl = "https://lampinboxportal.azurewebsites.net";
+    const url = "https://lampinboxportal.azurewebsites.net/api/v1/dynamic/dataoperation/get-recents-products-list";
+    useEffect(()=>{
+        axios.post(url,{
+            "requestParameters":{
+                "PageNo": 1,
+                "PageSize": 100,
+                "recordValueJson": "[]"
+            }
+        }).then(res=>{
+            console.log('response',JSON.parse(res.data.data))
+            setData(JSON.parse(res.data.data));
+        }).catch(e=>{
+            console.log('error',e)
+        })
+
+    },[])
   return (
-    <CategorySectionCreator title="Popular Items" seeMoreLink="#">
+    <CategorySectionCreator title="Recent Items" seeMoreLink="#">
       <Carousel
         infinite={true}
         visibleSlides={visibleSlides}
@@ -39,15 +58,15 @@ const GiftShopPopularItems = ({ productsData }) => {
           },
         }}
       >
-        {productsData.map((item, ind) => (
+        {data.map((item, ind) => (
           <ProductCard16
             key={ind}
-            id={item.id}
-            title={item.title}
-            price={item.price}
-            off={item.discount}
-            rating={item.rating}
-            imgUrl={item.imgUrl}
+            id={item.ProductId}
+            title={item.ProductName}
+            price={item.Price}
+            off={item.IsDiscountAllowed?10:0}
+            rating={item.Rating}
+            imgUrl={`${domainUrl}${item.ProductImagesJson[0].AttachmentURL}`}
           />
         ))}
       </Carousel>
